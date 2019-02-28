552458558b3c71660569e10d6ccb434c6cc91cbc
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2019-02-28 14:37:35 +0000

    completed MVP

--- src/main/java/com/example/CourseBooking/repositories/BookingRepository.java
+++ src/main/java/com/example/CourseBooking/repositories/BookingRepository.java
@@ -3,5 +3,9 @@ package com.example.CourseBooking.repositories;
 import com.example.CourseBooking.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Long>, BookingRepositoryCustom {
+    List<Booking> getAllBookingsByDate(String date);
+
 }
